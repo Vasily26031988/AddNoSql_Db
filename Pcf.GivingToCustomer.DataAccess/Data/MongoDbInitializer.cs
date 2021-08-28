e5a547e3b038b50be0e6e5bda0ab4938f4d8b613
Vasily Gololobov <Sator1988@gmail.com>
2021-08-28 19:12:11 +0400

    last changes

---
+++ Pcf.GivingToCustomer.DataAccess/Data/MongoDbInitializer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using Pcf.GivingToCustomer.Core.Domain;
+
+namespace Pcf.GivingToCustomer.DataAccess.Data
+{
+    public class MongoDbInitializer : IDbInitializer
+    {
+        private readonly IMongoCollection<Preference> _preferenceCollection;
+        private readonly IMongoCollection<Customer> _customerCollection;
+        private readonly IMongoCollection<PromoCode> _promoCodeCollection;
+
+        public MongoDbInitializer(IMongoCollection<Preference> preferenceCollection,
+            IMongoCollection<Customer> customerCollection,
+            IMongoCollection<PromoCode> promoCodeCollection)
+        {
+            _preferenceCollection = preferenceCollection;
+            _customerCollection = customerCollection;
+            _promoCodeCollection = promoCodeCollection;
+        }
+
+
+        public void InitializeDb()
+        {
+            _promoCodeCollection.DeleteMany(x => true);
+            _customerCollection.DeleteMany(x => true);
+            _preferenceCollection.DeleteMany(x => true);
+
+            _preferenceCollection.InsertMany(FakeDataFactory.Preferences);
+            _customerCollection.InsertMany(FakeDataFactory.Customers);
+        }
+    }
+}
